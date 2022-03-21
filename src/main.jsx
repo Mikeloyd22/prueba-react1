1d35ef75183b3726220af6651f85b4fd6eeafb59
Mikeloyd22 <100964003+Mikeloyd22@users.noreply.github.com>
2022-03-21 10:30:26 +0100

    Set base

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={ <Home></Home>}/>
+      <Route path='/facturas' element={ <Facturas></Facturas>}/>
+      <Route path='/productos' element={ <Productos></Productos>}/>
+      <Route path='/clientes' element={ <Clienes></Clienes>}/>
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 )
